6332fe8f1127622ec2180a418ee95646082a6188
Muhimin <codewithrakib30@gmail.com>
2024-01-05 11:42:57 +0600

    Prime Factirization with sieve

---
+++ prime_factorization.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//sieve code;
+const int sz = 1e7 + 23;
+int mx = 1e7;
+bitset<sz> is_prime;
+vector<int> primes;
+ 
+void prime(){
+	for(int i = 3; i <= mx; i += 2){
+		is_prime[i] = 1;
+	}
+	for(int i = 3; i <= sqrt(mx); i += 2){
+		for(int j = i * i; j <= mx; j += i){
+			is_prime[j] = 0;
+		}
+	}
+	primes.push_back(2);
+
+	for(int i = 3; i <= mx; i++){
+		if(is_prime[i]){
+			primes.push_back(i);
+		}
+	}
+ 
+}
+
+// can generate upto n == 1e14; 
+// return vector of all prime factoriz
+vector<int> primefac(long long n){
+	vector<int> ans;
+
+	for(auto a: primes){
+
+		if(1LL * a * a > n) break;
+
+		if(n % a == 0){
+			while(n % a == 0){
+				ans.push_back(a);
+				n /= a;
+			}
+		}
+	}
+
+	if(n > 1){
+		ans.push_back(n);
+	}
+	return ans;
+}
+
+int main()
+{	
+	prime();
+
+	return 0;
+}
